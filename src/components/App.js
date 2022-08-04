7e8e8c42a2322a620ad6aa78ae9986b0b794b6f0
gatalsky-dv <gatalsky.d.v@gmail.com>
2022-08-04 22:30:32 +0500

    Completed closing popups by button

--- src/components/App.js
+++ src/components/App.js
@@ -8,27 +8,27 @@ import PopupWithForm from './PopupWithForm.js';
 import ImagePopup from './ImagePopup.js';
 
 export default function App() {
-  function handleEditAvatarClick() {
-    document.querySelector('.popup_avatar').classList.add('popup_opened');
-  }
   
-  function handleEditProfileClick() {
-    document.querySelector('.popup_user').classList.add('popup_opened');
-  }
-  
-  function handleAddPlaceClick() {
-    document.querySelector('.popup_card').classList.add('popup_opened');
-  }
-
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
+  
+  function handleEditProfileClick() {
+    setIsEditProfilePopupOpen(true);
+  }
+  
+  function handleAddPlaceClick() {
+    setIsAddPlacePopupOpen(true);
+  }
+  
+  function handleEditAvatarClick() {
+    setIsEditAvatarPopupOpen(true);
+  }
 
   function closeAllPopups() {
     setIsEditProfilePopupOpen(false);
     setIsAddPlacePopupOpen(false);
     setIsEditAvatarPopupOpen(false);
-    console.log('работает');
   }
   
   return (
@@ -45,18 +45,81 @@ export default function App() {
         title='Редактировать профиль'
         isOpen={isEditProfilePopupOpen}
         onClose={closeAllPopups}
+        children = {
+          <>
+          <input
+            type="text"
+            className="popup__input popup__input_value_name"
+            id="name-input"
+            name="user"
+            placeholder="Имя позьзователя"
+            required
+            minLength="2"
+            maxLength="40"
+          />
+          <span className="name-input-error"></span>
+          <input
+            type="text"
+            className="popup__input popup__input_value_job"
+            id="job-input"
+            name="job"
+            placeholder="О себе"
+            required
+            minLength="2"
+            maxLength="200"
+          />
+          <span className="job-input-error"></span>
+          </>
+        }
       />
       <PopupWithForm
         name='card'
         title='Новое место'
         isOpen={isAddPlacePopupOpen}
         onClose={closeAllPopups}
+        children = {
+          <>
+            <input
+              type="text"
+              className="popup__input popup__input_value_title"
+              id="title-input"
+              name="name"
+              placeholder="Название"
+              required
+              minLength="2"
+              maxLength="30"
+            />
+            <span className="title-input-error"></span>
+            <input
+              type="url"
+              className="popup__input popup__input_value_link"
+              id="link-input"
+              name="link"
+              placeholder="Ссылка на картинку"
+              required
+            />
+            <span className="link-input-error"></span>
+          </>
+        }
       />
       <PopupWithForm
         name='avatar'
         title='Обновить аватар'
         isOpen={isEditAvatarPopupOpen}
         onClose={closeAllPopups}
+        children ={
+          <>
+            <input
+              type="url"
+              className="popup__input popup__input_value_link"
+              id="avatar-input"
+              name="avatar"
+              placeholder="Ссылка на аватарку"
+              required
+            />
+            <span className="avatar-input-error"></span>
+          </>
+        }
       />
       <PopupWithForm
         name='confirmation'
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -8,6 +8,7 @@ export default function PopupWithForm(props) {
 				<button aria-label="Закрыть" type="button" className={`popup__close popup__close_${props.name}`} onClick={props.onClose} />
 				<h3 className="popup__title">{props.title}</h3>
 				<form name="profile" method="get" className={`popup__form popup__form_${props.name}`} noValidate>
+					{props.children}
 					<button type="submit" className="popup__button">Сохранить</button>
 				</form>
 			</div>
